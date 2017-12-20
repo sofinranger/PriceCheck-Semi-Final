e9d507427d40aa435095578863ac767eb7fc49c8
sofinranger <sofinranger@gmail.com>
2017-12-20 14:41:01 +0700

    SCAN BARCODE
    Scan Barcode Berhasil

--- app/src/main/java/com/example/sf/pricecheck/MainActivity.java
+++ app/src/main/java/com/example/sf/pricecheck/MainActivity.java
@@ -1,33 +1,69 @@
 package com.example.sf.pricecheck;
 
-import android.content.Context;
-import android.content.DialogInterface;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import google.zxing.integration.android.IntentIntegrator;
+import google.zxing.integration.android.IntentResult;
 
-    Context context = this;
+public class MainActivity extends Activity implements View.OnClickListener {
+//    Context context = this;
+//    Button PesanOK;
 
-    Button PesanOK;
+    private Button scanBtn;
+    private TextView  contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // =========================MODULE PESAN OK SAJA
-        PesanOK = (Button) findViewById(R.id.BtnCari);
-        PesanOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                builder.setTitle(Constants.JUDUL_PESAN);
-                builder.setMessage(Constants.PESAN_1);
+        scanBtn = (Button)findViewById(R.id.BtnScan);
+        contentTxt = (TextView)findViewById(R.id.TxtKode);
+
+        scanBtn.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+        if(v.getId()==R.id.BtnScan){
+            //scan
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            //we have a result
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            contentTxt.setText(scanContent);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    //        // =========================MODULE PESAN OK SAJA
+//        PesanOK = (Button) findViewById(R.id.BtnCari);
+//        PesanOK.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//                builder.setTitle(Constants.JUDUL_PESAN);
+//                builder.setMessage(Constants.PESAN_1);
 
 //                // Membuat tombol negativ
 //                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -44,9 +80,9 @@ public class MainActivity extends AppCompatActivity {
 //                        Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
 //                    }
 //                });
-                builder.show();
-            }
-        });
-        // =========================END MODULE PESAN OK SAJA
-    }
+//                builder.show();
+//            }
+//        });
+//        // =========================END MODULE PESAN OK SAJA
+
 }
